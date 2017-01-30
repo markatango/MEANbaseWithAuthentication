52ddeece7598a27c321c20ae2411cdd25f7c3667
Mark Rodighiero <markatango@gmail.com>
2017-01-29 22:07:33 -0800

    moved cors.js to custom folder

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ var mongoose = require('./config/mongoose'), // load mongoose first
     express = require('./config/express'),
     passport = require('./config/passport');
 
+// for mongoStore for socket.io
 var db = mongoose();
 var app = express(db); // pass db to express for mongoStore
 var passport = passport();
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT); // see express.js, which returns `server`. this the thing that `listens`.
 module.exports = app;  // pass it along 
 
 console.log('Server running at http://localhost:' + process.env.PORT);
